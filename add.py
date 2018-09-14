cc0fdd8bda58dba8662f2561aef0088742f4cef0
harsha7272 <42933880+harsha7272@users.noreply.github.com>
2018-09-14 12:53:04 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,42 @@
+'''a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+c=a-b
+print(c)'''
+
+'''x=int(input("enter the value of x:"))
+y=int(input("enter the value of y:"))
+z=x+y
+print(z)'''
+
+
+'''g=int(input("enter the value of g:"))
+h=int(input("enter the value of h:"))
+i=g*h
+print(i)
+
+gx=int(input("enter the value of gx:"))
+hy=int(input("enter the value of hy:"))
+i=gx/hy
+print(i)'''
+
+'''gh=int(input("enter the value of gh:"))
+hk=int(input("enter the value of hk:"))
+i=gh**hk
+print(i)'''
+
+'''gu=int(input("enter the value of gu:"))
+hn=int(input("enter the value of hn:"))
+i=gu/hn
+print(c)'''
+
+'''x=int(input("enter the value of x:"))
+z=int(input("enter the value of z:"))
+j=x>z
+print(j)'''
+
+a=int(input("enter a number:"));
+if(a%2==0):
+	print("the number is even")
+else:
+	print("the number is odd")
+
