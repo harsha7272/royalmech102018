04259bed6fdff04862e2feef6e2edcacaed82d85
harsha7272 <42933880+harsha7272@users.noreply.github.com>
2018-09-17 16:23:11 +0530

    Add files via upload

---
+++ ll.py
@@ -0,0 +1,38 @@
+'''a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+if(a>b):
+	print("a is greater than b")
+else:
+	print("b is greater than a")'''
+
+	
+'''a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=((a**2)-(2*a*b)+(b**2))
+print(c)'''
+
+'''marks=int(input("enter your marks"))
+if(marks>=90):
+	print("a+")
+elif(marks>=80):
+	print("a")
+elif(marks>=70):
+	print("b+")
+elif(marks>=60):
+	print("b")
+elif(marks>=50):
+	print("c")
+elif(marks>=40):
+	print("d")
+elif(marks<40):
+	print("!!!!!!! YOU FAILED !!!!!!")'''
+
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+c=int(input("enter the value of c:"))
+if(a>b & a>c):
+	print("a is the greatest number")
+elif(b>c):
+	print("b is the greatest number")
+else:	
+	print("c is the greatest number")
