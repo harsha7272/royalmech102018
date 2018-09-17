638fdfdc5e2e984a529a2e473bdf56ef752e157f
harsha7272 <42933880+harsha7272@users.noreply.github.com>
2018-09-17 15:42:45 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,28 @@
+'''a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+if(a>b):
+	print("a is greater than b")
+else:
+	print("b is greater than a")'''
+
+	
+'''a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=((a**2)-(2*a*b)+(b**2))
+print(c)'''
+
+marks=int(input("enter your marks"))
+if(marks>90):
+	print("a+")
+elif(marks>80):
+	print("a")
+elif(marks>70):
+	print("b+")
+elif(marks>60):
+	print("b")
+elif(marks>50):
+	print("c")
+elif(marks<40):
+	print("FAIL!!!!!")
+
+
