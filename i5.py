f6a738afbe02cde90289fbf65033736c4e71255c
harsha7272 <42933880+harsha7272@users.noreply.github.com>
2018-09-24 16:28:17 +0530

    Add files via upload

---
+++ i5.py
@@ -0,0 +1,20 @@
+'''i=1
+while(12<i<20):
+	print(i)'''
+	
+"""a=float(input("Enter a Number:"));
+if(a%2==0):
+	print("This Number is Even")
+else:
+	print("This Number is Odd")"""
+'''import random
+r=random.randint(1,6)
+print(r)'''
+
+import random
+while True :
+	a=input("press t")
+
+
+
+
