59c5904923ed250bcd4bd6d3a19bab1098f0b845
harsha7272 <42933880+harsha7272@users.noreply.github.com>
2018-11-05 16:16:49 +0530

    Add files via upload

---
+++ any.py
@@ -0,0 +1,10 @@
+class student:
+    age=21
+    	name='harsha'
+    def display(ob):
+        c=ob.age
+        d=ob.name
+        print(c,'\n',d)
+a=student()
+a.display()           
+
---
+++ pract.py
@@ -0,0 +1,9 @@
+
+class demo:
+	x=8
+	x=9
+	def add(ob):
+	c=ob.x+ob.y
+	print(c)
+print(p.x,p.y)
+p.add() 
